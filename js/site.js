fe70079c4a2f1a6c9200e7d1addd310515b73329
ShawnRogoff <shawnrogoff@gmail.com>
2021-07-24 15:13:07 -0500

    Initial project completion

--- js/site.js
+++ js/site.js
@@ -1,4 +1,42 @@
-// Call Hello World
-function HelloWorld(){
-    alert("Hello World!");
+// Get the string value from page
+// Controller Function
+function getValue(){
+    // ensures the alert is invisible when the page starts
+    document.getElementById("alert").classList.add("invisible");
+
+    // set variable to the string users put into input field
+    let userString = document.getElementById("userString").value;
+
+    // pass string into reverseString function
+    let reversedString = reverseString(userString);
+
+    // pass reversed string to displayString function
+    displayString(reversedString);
+}
+
+// Reverse the string
+// Logic Function
+function reverseString(userString){
+    let reversedString = [];
+
+    // use a for loop to reverse the string
+    for (let index = userString.length - 1; index >= 0; index--) {
+        reversedString += userString[index];
+    }
+
+    // return the reversedString array to getValue function
+    return reversedString;
+}
+
+// Display the reversed string to page
+// View Function
+function displayString(reversedString){
+
+    // write the message to the page
+
+    document.getElementById("message").innerHTML = `"The reversed string is: ${reversedString}"`;
+
+    // show the alert box
+
+    document.getElementById("alert").classList.remove("invisible");
 }
